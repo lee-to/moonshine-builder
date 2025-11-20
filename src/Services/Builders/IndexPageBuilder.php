01bbb7a25d6627d8b0f392bedee0c00f04589882
dev-lnk <lkivanmail@gmail.com>
2025-11-20 22:05:15 +0300

    IndexPage builder

--- src/Services/Builders/IndexPageBuilder.php
+++ src/Services/Builders/IndexPageBuilder.php
@@ -17,10 +17,56 @@ class IndexPageBuilder extends AbstractBuilder implements IndexPageBuilderContra
 {
     private MoonShineStructure $moonShineStructure;
 
+    /**
+     * @throws FileNotFoundException
+     * @throws ProjectBuilderException
+     */
     public function build(): void
     {
+        $this->moonShineStructure = new MoonShineStructure($this->codeStructure);
+
         $pagePath = $this->codePath->path(BuildType::INDEX_PAGE->value);
 
-        StubBuilder::make($this->stubFile)->makeFromStub($pagePath->file());
+        StubBuilder::make($this->stubFile)->makeFromStub($pagePath->file(), [
+            '{namespace}' => $pagePath->namespace(),
+            '{class}' => $pagePath->rawName(),
+            '{name}' => str_replace('IndexPage', '', $pagePath->rawName()),
+            '{field_uses}' => $this->usesFieldsToResource(),
+            '{fields}' => $this->columnsToResource(),
+            '{filters}' => $this->columnsToResource(onlyFilters: true),
+        ]);
+    }
+
+    /**
+     * @throws ProjectBuilderException
+     */
+    protected function usesFieldsToResource(): string
+    {
+        $result = "";
+
+        foreach ($this->moonShineStructure->getUsesForFields() as $use) {
+            $result .= str($use)->newLine()->value();
+        }
+
+        return $result;
+    }
+
+    /**
+     * @throws ProjectBuilderException
+     */
+    protected function columnsToResource(bool $onlyFilters = false): string
+    {
+        $result = "";
+
+        foreach ($this->moonShineStructure->getFields(tabulation: 4, onlyFilters: $onlyFilters) as $field) {
+            $result .= str($field)
+                ->prepend("\t\t\t")
+                ->prepend("\n")
+                ->append(',')
+                ->value()
+            ;
+        }
+
+        return $result;
     }
 }
