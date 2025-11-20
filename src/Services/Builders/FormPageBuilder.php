548341618d3794dbc1354e1a1186cbdadc1f8f0a
dev-lnk <lkivanmail@gmail.com>
2025-11-20 22:22:03 +0300

    FormPage builder

--- src/Services/Builders/FormPageBuilder.php
+++ src/Services/Builders/FormPageBuilder.php
@@ -12,14 +12,24 @@ use DevLnk\MoonShineBuilder\Services\CodeStructure\MoonShineStructure;
 use DevLnk\MoonShineBuilder\Services\StubBuilder;
 use Illuminate\Contracts\Filesystem\FileNotFoundException;
 
-class FormPageBuilder extends AbstractBuilder implements FormPageBuilderContract
+class FormPageBuilder extends PageBuilder implements FormPageBuilderContract
 {
     private MoonShineStructure $moonShineStructure;
 
     public function build(): void
     {
         $pagePath = $this->codePath->path(BuildType::FORM_PAGE->value);
+        $resourcePath = $this->codePath->path(BuildType::RESOURCE->value);
 
-        StubBuilder::make($this->stubFile)->makeFromStub($pagePath->file());
+        StubBuilder::make($this->stubFile)->makeFromStub($pagePath->file(), [
+            '{namespace}' => $pagePath->namespace(),
+            '{resource_namespace}' => $resourcePath->namespace(),
+            '{class}' => $pagePath->rawName(),
+            '{name}' => str_replace('FormPage', '', $pagePath->rawName()),
+            '{resource_name}' => $resourcePath->rawName(),
+            '{field_uses}' => $this->usesFieldsToResource(),
+            '{fields}' => $this->columnsToResource(),
+            '{rules}' => $this->columnsToRules(),
+        ]);
     }
 }
--- src/Services/Builders/IndexPageBuilder.php
+++ src/Services/Builders/IndexPageBuilder.php
@@ -4,69 +4,34 @@ declare(strict_types=1);
 
 namespace DevLnk\MoonShineBuilder\Services\Builders;
 
-use Cassandra\Index;
 use DevLnk\MoonShineBuilder\Enums\BuildType;
 use DevLnk\MoonShineBuilder\Exceptions\ProjectBuilderException;
 use DevLnk\MoonShineBuilder\Services\Builders\Contracts\IndexPageBuilderContract;
-use DevLnk\MoonShineBuilder\Services\Builders\Contracts\ResourceBuilderContract;
-use DevLnk\MoonShineBuilder\Services\CodeStructure\MoonShineStructure;
 use DevLnk\MoonShineBuilder\Services\StubBuilder;
 use Illuminate\Contracts\Filesystem\FileNotFoundException;
 
-class IndexPageBuilder extends AbstractBuilder implements IndexPageBuilderContract
+class IndexPageBuilder extends PageBuilder implements IndexPageBuilderContract
 {
-    private MoonShineStructure $moonShineStructure;
-
     /**
      * @throws FileNotFoundException
      * @throws ProjectBuilderException
      */
     public function build(): void
     {
-        $this->moonShineStructure = new MoonShineStructure($this->codeStructure);
-
         $pagePath = $this->codePath->path(BuildType::INDEX_PAGE->value);
+        $resourcePath = $this->codePath->path(BuildType::RESOURCE->value);
 
         StubBuilder::make($this->stubFile)->makeFromStub($pagePath->file(), [
             '{namespace}' => $pagePath->namespace(),
+            '{resource_namespace}' => $resourcePath->namespace(),
             '{class}' => $pagePath->rawName(),
             '{name}' => str_replace('IndexPage', '', $pagePath->rawName()),
+            '{resource_name}' => $resourcePath->rawName(),
             '{field_uses}' => $this->usesFieldsToResource(),
             '{fields}' => $this->columnsToResource(),
             '{filters}' => $this->columnsToResource(onlyFilters: true),
         ]);
     }
 
-    /**
-     * @throws ProjectBuilderException
-     */
-    protected function usesFieldsToResource(): string
-    {
-        $result = "";
 
-        foreach ($this->moonShineStructure->getUsesForFields() as $use) {
-            $result .= str($use)->newLine()->value();
-        }
-
-        return $result;
-    }
-
-    /**
-     * @throws ProjectBuilderException
-     */
-    protected function columnsToResource(bool $onlyFilters = false): string
-    {
-        $result = "";
-
-        foreach ($this->moonShineStructure->getFields(tabulation: 4, onlyFilters: $onlyFilters) as $field) {
-            $result .= str($field)
-                ->prepend("\t\t\t")
-                ->prepend("\n")
-                ->append(',')
-                ->value()
-            ;
-        }
-
-        return $result;
-    }
 }
